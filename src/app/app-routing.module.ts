c59d23004a43fa97dff1829d1fc175db906cc669
Babbaji <gabri.babba@gmail.com>
2019-03-16 15:50:31 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: './layout/standard-layout.module#StandardLayoutModule'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
